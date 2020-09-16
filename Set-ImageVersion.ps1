6035b62e8fd6fc443be9351893e1f7ae2ad2848e
Pawel1985 <pkondrathome@gmail.com>
2020-09-16 15:45:38 +0200

    Update Set-ImageVersion.ps1

--- Set-ImageVersion.ps1
+++ Set-ImageVersion.ps1
@@ -1,1 +1,5 @@
+$Date = get-date -Format yyyy-MM-dd--HH:mm
+New-Item -Path HKLM:\SOFTWARE -Name HAYS-IT -ItemType Directory -Force
+New-Item -Path HKLM:\SOFTWARE\HAYS-IT -Name Buildinfo -ItemType Directory -Force
+New-ItemProperty -Path HKLM:\SOFTWARE\HAYS-IT\Buildinfo\ -Name ImageVersion -Value $Date -PropertyType STRING
 
